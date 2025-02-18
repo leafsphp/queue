323c4cc5870701ab9b618da58b2894f93407e05d
mychidarko <mickdd22@gmail.com>
2025-02-18 17:35:56 +0000

    feat: add support for db adapter

---
+++ src/Queue/Dispatchable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Leaf\Queue;
+
+/**
+ * Contract for dispatchable classes
+ */
+interface Dispatchable
+{
+    /**
+     * Return configured connection
+     */
+    public function connection();
+
+    /**
+     * Add data to the job
+     */
+    public function with($data);
+
+    /**
+     * Return job stack if available
+     */
+    public function stack();
+
+    /**
+     * Return config for the job
+     * @return array
+     */
+    public function getConfig();
+}
